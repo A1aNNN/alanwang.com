7f2783f6eef98c9437cb0c838c28d520a433d6ac
Alan Wang <alanwang123919@Alans-iMac.local>
2022-11-23 23:57:25 -0800

    second commit

---
+++ src/components/Experience.js
@@ -0,0 +1,17 @@
+import { Text } from '@chakra-ui/react'
+import React from 'react'
+import ExperienceCard from './ExperienceCard'
+
+const Experience = () => {
+  return (
+    <div>
+      <Text>Experience</Text>
+
+      <ExperienceCard></ExperienceCard>
+
+      <ExperienceCard></ExperienceCard>
+    </div>
+  )
+}
+
+export default Experience
