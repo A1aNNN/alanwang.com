41000a3048da229267b9fc5aa929fccbf1aad82b
Alan Wang <alanwang123919@gmail.com>
2023-01-09 15:57:43 -0800

    added headshot

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,14 +3,15 @@ import React from 'react'
 
 const Intro = () => {
   return (
-    <Box display="flex" flexDirection="column" padding="20px" paddingBottom="150px">
-     <Text alignSelf="center" className="title" id='aboutMe'>
+    <Box display="flex" flexDirection="column" padding="20px" paddingBottom="160px" paddingTop="80px">
+     <Text alignSelf="center" className="title" id='aboutMe' paddingBottom="40px">
       ~ About me ~
      </Text>
      
      <Text className='aboutMeContent'>
         A 4th year UBC biochem student, just trying his best out here. I currently work in bioinformatics studying endogenous retroviruses,
          but I'm looking to venture out and expand into the world of tech!
+        <br/>
         <br/>
         Being a retired swimmer, my hobbies outside of academics and work highlight the idea of staying active. Catch me
          at the gym doing anything but cardio! 
--- src/components/Titlename.js
+++ src/components/Titlename.js
@@ -1,20 +1,20 @@
 import { Box, Image, Text} from '@chakra-ui/react'
 import { Highlight } from '@chakra-ui/react'
 import React from 'react'
-import Headshot from "../images/52657236.png"
 import WordsTypewriter from './WordsTypewriter';
+import Alan from "/Users/alanbyron/Desktop/alanwang.com-master/src/images/alanheadshot.jpg"
 
 const Titlename = () => {
   return (
-    <Box display="flex" flexDirection="row" padding="80px 0px 25px">
-      <Box display="flex" flexDirection="column" alignItems="center">
+    <Box display="flex" flexDirection="row" padding="80px 0px 25px" alignItems="center" >
+      <Box display="flex" flexDirection="column" alignItems="center" w={{base: "600px", md: "600px", lg: "950px"}}>
         <Text className='name'>Hi, my name is</Text>
         <Text fontWeight="bold" className='name highlight'>Alan Wang.</Text>
         <WordsTypewriter/>
       </Box>
-
-{/* Fix this squished image */}
-      {/* <Image src={Headshot} borderRadius="100%" boxSize="150px" position="absolute" right="200px"/> */}
+      <Box>
+        <Image src={Alan} alt='Alan Headshot' boxSize="400px" objectFit="cover" borderRadius="full"/>
+      </Box>
     </Box>
   )
 }
